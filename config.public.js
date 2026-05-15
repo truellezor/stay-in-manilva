de612b7e20c5058e072158555b8e60118dd59b07
truellezor <truellezor@gmail.com>
2026-05-15 22:56:23 +0300

    Configure Supabase production storage

--- config.public.js
+++ config.public.js
@@ -1,1 +1,4 @@
-window.STAY_IN_MANILVA_SUPABASE = null;
+window.STAY_IN_MANILVA_SUPABASE = {
+  url: "https://pmozhxfnhbsitwkqtyzt.supabase.co",
+  anonKey: "sb_publishable_XzyCBsym691A263hHOJFVQ_w3GFr7vI"
+};
